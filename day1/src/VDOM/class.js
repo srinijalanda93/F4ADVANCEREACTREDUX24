9784c9fa15ca1be5d33d4a27841deb4400216c6b
srinija landa <srinijalanda@gmail.com>
2024-03-20 13:34:35 +0530

    state of class

--- day1/src/VDOM/class.js
+++ day1/src/VDOM/class.js
@@ -1,4 +1,4 @@
-import {Component} from "react";
+import { Component } from "react";
 //class component in Javascript
 // function App() {
 //   return (
@@ -10,11 +10,46 @@ import {Component} from "react";
 //   );
 // }
 
+//for the functional componet no need of the Component class and
+//render() method
 //using about functional code we need to convert into class Component
 //for this we neet to import the Component from the React
-export class App extends Component{
-
+//to make a class component we need to inherit Component class using extend
+/**
+ * 1.for class component we need to use render() which returns the jsx
+ * 2.to access the props we need to implement the constructor
+ * 3.state is predefined inside the Component alway in object format we can dothe change of this.state inside the constructor
+ */
+export class App extends Component {
+  //constructor for the accessing the props
+  /**never do this "state" is only read ony value direct update can't be done
+   * this.state={count:1}
+   * this.state.count+=1; 
+   */
+  constructor(props) {
+    super(props);
+    console.log(props);
+    //so inside the Component we have "state which in the object form"
+    this.state = { count: 0 };
+  }
+  /**
+   * inside the class component  <App name="hello! girls"  edc="sfc"/> =>new App({name:"srinija",edc:"sfc"})
+   * inside the functional component   <App name="hello! girls"  edc="sfc"/> =>App({name:"srinija",edc:"sfc"})
+   */
+  render() {
+    return (
+      <>
+        <div>
+          
+          <h1>
+            Name:{this.props.name} ,age:{this.props.age},count:{" "}
+            {this.state.count}
+          </h1>
+        </div>
+      </>
+    );
+  }
 }
 
-
-
+//function component
+//const App =({name} =><h1>Srinija</h1>)
--- day1/src/index.js
+++ day1/src/index.js
@@ -1,21 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
- import {navbar} from "./ModulesTopic/navbar"
+//import "./index.css";
+//import {navbar} from "./ModulesTopic/navbar"
 // import {Product} from "./ModulesTopic/product"
-import {Information} from "./ModulesTopic/navbar"
+//import {Information} from "./ModulesTopic/navbar";
+import { App } from "./VDOM/class";
 
 //const h1=React.createElement("h2",{id:"s"},"helloworld!");
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-const url="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/assets/products/sliding_images/jpeg/93579704-2486-4199-8e63-cc3436f3fae5.jpg?ts=1708593038";
-let cost=50
-let title="Britannia Milk Bread"
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// const url="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/assets/products/sliding_images/jpeg/93579704-2486-4199-8e63-cc3436f3fae5.jpg?ts=1708593038";
+// let cost=50
+// let title="Britannia Milk Bread"
+// root.render(
+// <>
+// <Information Imageurl={url} altName="Blinkit"  title={title} weight="400g" price={cost}/>
+// </>
+// )
+
 root.render(
-<>
-<Information Imageurl={url} altName="Blinkit"  title={title} weight="400g" price={cost}/>
-</>
+  <>
+    <App name="hello! girls"  edc="sfc"/>
+  </>
 );
-
-
-
